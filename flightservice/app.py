33aa9ad948fe53b5fff604354c5281bc3dd2e8ff
EMIRATES\S411513 <vijaykumar@emirates.com>
2020-04-24 18:07:43 +0400

    Base Line Project

---
+++ flightservice/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from parserservice.parseurl import Pareser
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/parseUrl',methods=['GET'])
+def parseDataFronURL():
+    # inputReq=request.data;
+    parse=Pareser('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat')
+    parse.pareseDataFromURL();
+    return ""
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port='8443')
+    parseDataFronURL()
